43b7b44b0badd268928223d9f95a783b63d01ef0
rebecca <becky@strategically.co>
2021-05-17 14:49:04 +0100

    home copy

--- src/sections/home3/Hero.js
+++ src/sections/home3/Hero.js
@@ -15,11 +15,11 @@ const Hero = ({className,...rest}) => {
 
               >
                 <h1 className="font-size-11 text-white mb-7">
-                Strategically helps brands become publishers.
+                Get Your Content Creation Team in a Few Clicks
                 </h1>
                 <p className="font-size-7 px-lg-5 mb-0">
-                We create content that audiences will share, link to, engage with, return to, and—most importantly—value more than your competitors.</p>
-                <div className="btn-group d-inline-block mt-10">
+                Your on-demand content creation service. Simple, fast and affordable.</p>
+              <div className="btn-group d-inline-block mt-10">
                   <a className="btn btn-buttercup btn-2 rounded-5" href={"/pricing"}>
                     View Pricing
                   </a>
